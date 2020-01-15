d80d30569a2b4ab578bf76a4d29e6c2b3842d9d4
AukGuisesTike <keisuke.sugita@jp.ricoh.com>
2020-01-15 17:40:18 +0900

    例外テストの追加

--- codes/SeatMonitoringAPI/SeatMonitoringAPITest/ConfigurationTest.cs
+++ codes/SeatMonitoringAPI/SeatMonitoringAPITest/ConfigurationTest.cs
@@ -12,16 +12,47 @@ namespace SeatMonitoringAPITest
         [TestMethod]
         public void TestMethod1()
         {
-            var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(@"[{""Moniker"":""USB#VID_046D&PID_0826&MI_02#6&24BF100&0&0002"",""Name"":""杉田 圭輔""},
-{""Moniker"":""USB#VID_046D&PID_0826&MI_02#6&24BF100&0&0003"",""Name"":""Keisuke Sugita""}]"));
+            try
+            {
+                var configuration = Configuration.Instance.SeatDefinitions;
+            }
+            catch(InvalidOperationException e)
+            {
+                Assert.AreEqual(e.Message, "Configurationが初期化されていません。");
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(@"[{""Moniker"":""usb#vid_046d&pid_0826&mi_02#6&24bf100&0&0002"",""Name"":""杉田 圭輔""},
+{""Moniker"":""usb#vid_046d&pid_0826&mi_02#6&24bf100&0&0003"",""Name"":""Keisuke Sugita""}]"));
             using (var streamReader = new StreamReader(memoryStream))
             {
-                Configuration.initialize(streamReader);
+                Configuration.Initialize(streamReader);
                 var configuration = Configuration.Instance.SeatDefinitions;
-                Assert.AreEqual(configuration[0].Moniker, "USB#VID_046D&PID_0826&MI_02#6&24BF100&0&0002");
+                Assert.AreEqual(configuration[0].Moniker, "usb#vid_046d&pid_0826&mi_02#6&24bf100&0&0002");
                 Assert.AreEqual(configuration[0].Name, "杉田 圭輔");
-                Assert.AreEqual(configuration[1].Moniker, "USB#VID_046D&PID_0826&MI_02#6&24BF100&0&0003");
+                Assert.AreEqual(configuration[1].Moniker, "usb#vid_046d&pid_0826&mi_02#6&24bf100&0&0003");
                 Assert.AreEqual(configuration[1].Name, "Keisuke Sugita");
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(@"[{""Moniker"":""usb#vid_046d&pid_0826&mi_02#6&24bf100&0&0002"",""Name"":""杉田 圭輔""},
+{""Moniker"":""usb#vid_046d&pid_0826&mi_02#6&24bf100&0&0003"",""Name"":""Keisuke Sugita""}]"));
+            using (var streamReader = new StreamReader(memoryStream))
+            {
+                try
+                {
+                    Configuration.Initialize(streamReader);
+                }
+                catch(InvalidOperationException e)
+                {
+                    Assert.AreEqual(e.Message, "Configurationは既に初期化されています。");
+                }
             }
         }
     }
