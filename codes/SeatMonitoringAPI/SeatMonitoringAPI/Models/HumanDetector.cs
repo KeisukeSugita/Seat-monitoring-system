7c2db4b25f51509f1d6ce80e290121519c811837
AukGuisesTike <keisuke.sugita@jp.ricoh.com>
2020-01-15 09:10:53 +0900

    配列への変換方法を変更　ポインタを取得して渡す

--- codes/SeatMonitoringAPI/SeatMonitoringAPI/Models/HumanDetector.cs
+++ codes/SeatMonitoringAPI/SeatMonitoringAPI/Models/HumanDetector.cs
@@ -11,25 +11,19 @@ namespace SeatMonitoringAPI.Models
 {
     public class HumanDetector
     {
-        [DllImport("HumanDetector.dll", CallingConvention = CallingConvention.Cdecl)]
-        static extern bool detect(int rows, int cols, byte[] image);
+        [DllImport("HumanDetector.dll", CallingConvention = CallingConvention.StdCall)]
+        extern static bool detect(int rows, int cols, IntPtr image);
         public bool Detect(Bitmap photo)
         {
-            using (MemoryStream memoryStream = new MemoryStream())
+            var bmpData = photo.LockBits(new Rectangle(0, 0, photo.Width, photo.Height), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+
+            try
             {
-                photo.Save(memoryStream, ImageFormat.Bmp);
-                byte[] image = memoryStream.ToArray();
-
-                byte[] headerOffset = { image[10], image[11], image[12], image[13] };
-                int offset = BitConverter.ToInt32(headerOffset, 0);
-
-                var convertedImage = new byte[photo.Height * photo.Width * 3];
-                for (int i = 0; i < convertedImage.Length; i++)
-                {
-                    convertedImage[i] = image[i + offset];
-                }
-
-                return detect(photo.Height, photo.Width, convertedImage);
+                return detect(photo.Height, photo.Width, bmpData.Scan0);
+            }
+            finally
+            {
+                photo.UnlockBits(bmpData);
             }
         }
     }
