8ceab5740bb244c74f3962b44fece442f77dd6f4
AukGuisesTike <keisuke.sugita@jp.ricoh.com>
2020-01-21 10:10:12 +0900

    stringの引数をSeatStatus型に変換してフィールドにセットするコンストラクタを追加

--- codes/座席監視アプリ/座席監視アプリ/Seat.cs
+++ codes/座席監視アプリ/座席監視アプリ/Seat.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,9 @@ namespace SeatMonitoringApplication
 {
     public class Seat
     {
+        private object name;
+        private SeatStatus seatStatus;
+
         public enum SeatStatus
         {
             Exists,
@@ -15,13 +19,42 @@ namespace SeatMonitoringApplication
             Failure
         }
 
+        [DataMember(Name = "Name")]
         public string Name { get; private set; }
+        [DataMember(Name = "Status")]
         public SeatStatus Status { get; private set; }
+
 
         public Seat(string name, SeatStatus status)
         {
             Name = name;
             Status = status;
+        }
+
+        public Seat(string name, string status)
+        {
+            Name = name;
+            Status = FromString(status);
+        }
+
+        public SeatStatus FromString(string status)
+        {
+            if (status == Seat.SeatStatus.Exists.ToString())
+            {
+                return Seat.SeatStatus.Exists;
+            }
+            else if (status == Seat.SeatStatus.NotExists.ToString())
+            {
+                return Seat.SeatStatus.NotExists;
+            }
+            else if (status == Seat.SeatStatus.Failure.ToString())
+            {
+                return Seat.SeatStatus.Failure;
+            }
+            else
+            {
+                throw new InvalidOperationException("指定された文字列はSetStatusに存在しません。");
+            }
         }
     }
 }
