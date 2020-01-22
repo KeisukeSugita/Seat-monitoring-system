d06813527b88e540623583d1db3cd187cedce858
AukGuisesTike <keisuke.sugita@jp.ricoh.com>
2020-01-22 12:17:35 +0900

    foreach文をlinqに書き換え Configurationが初期化できなかった場合にアプリケーションを終了する処理を追加

--- codes/座席監視アプリ/座席監視アプリ/Form1.cs
+++ codes/座席監視アプリ/座席監視アプリ/Form1.cs
@@ -12,22 +12,89 @@ namespace SeatMonitoringApplication
 {
     public partial class Form1 : Form
     {
+        private enum ToolTipTexts
+        {
+            [Description("在席")]
+            Exist,
+
+            [Description("離席")]
+            NotExist,
+
+            [Description("不明")] 
+            Unknown,
+
+            [Description("サーバ接続エラー")] 
+            ConectingError
+        }
+
+        private string GetText(ToolTipTexts toolTipText)
+        {
+            var member = toolTipText.GetType().GetMember(toolTipText.ToString());
+            var attributes = member[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+            var description = ((DescriptionAttribute)attributes[0]).Description;
+            return description;
+        }
+
+        private PeriodicNotifier PeriodicNotifier { get; set; }
         public Form1()
         {
-            InitializeComponent();
+            try
+            {
+                var ipAddress = Configuration.Instance.IpAddress;
+                PeriodicNotifier.Destination destination = Update;
+                PeriodicNotifier = new PeriodicNotifier(destination, new SeatMonitoringApiClient(ipAddress, new MyHttpClient()));
+            }
+            catch (InvalidOperationException e)
+            {
+                MessageBox.Show(e.Message, "起動エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(0);
+            }
 
+            InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             listView1.View = View.Details;
-            listView1.Clear();
-            listView1.Columns.Add("監視結果");
-            Update();
+            listView1.ShowItemToolTips = true;
+            PeriodicNotifier.Start();
         }
 
-        public void Update(List<Seat> seats)
+        private void Update(List<Seat> seats)
         {
+            Invoke(new Action<List<Seat>>(InvokeUpdate), seats);
+        }
+
+        private void InvokeUpdate(List<Seat> seats)
+        {
+            listView1.Items.Clear();
+
+            if (seats == null)
+            {
+                listView1.Items.Add(
+                    new ListViewItem("サーバへの接続に失敗しました。")
+                    {
+                        ImageIndex = (int)ToolTipTexts.ConectingError,
+                        ToolTipText = GetText(ToolTipTexts.ConectingError)
+                    }
+                    );
+            }
+            else
+            {
+                listView1.Items.AddRange(
+                    seats.Select(seat => new ListViewItem(seat.Name)
+                    {
+                        ImageIndex = (int)seat.Status,
+                        ToolTipText = GetText((ToolTipTexts)Enum.ToObject(typeof(ToolTipTexts), (int)seat.Status))
+                    })
+                    .ToArray()
+                    );
+            }
+        }
+
+        public void Close(object sender, EventArgs e)
+        {
+            PeriodicNotifier.Stop();
         }
     }
 }
