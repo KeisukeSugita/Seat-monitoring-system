0101be8b1460b4ae7c0da6352b8da07a95163b8f
AukGuisesTike <keisuke.sugita@jp.ricoh.com>
2020-01-22 12:19:00 +0900

    例外処理を修正

--- codes/座席監視アプリ/座席監視アプリ/SeatMonitoringApiClient.cs
+++ codes/座席監視アプリ/座席監視アプリ/SeatMonitoringApiClient.cs
@@ -9,53 +9,33 @@ using System.Threading.Tasks;
 
 namespace SeatMonitoringApplication
 {
-    public class SeatMonitoringApiClient
+    public class SeatMonitoringApiClient : ISeatMonitoringApiClient
     {
         private string IpAddress { get; set; }
+        private IMyHttpClient HttpClient { get; set; }
 
-        public SeatMonitoringApiClient(string ipAddress)
+        public SeatMonitoringApiClient(string ipAddress, IMyHttpClient httpClient)
         {
             IpAddress = ipAddress;
+            HttpClient = httpClient;
         }
 
         public List<Seat> GetSeats()
         {
-            using (var httpClient = new HttpClient())
+            HttpResponseMessage responseMessage;
+            HttpClient.Timeout = TimeSpan.FromMilliseconds(60000);
+            responseMessage = HttpClient.GetAsync($@"http://{IpAddress}:44383/api/seats");
+            if (responseMessage.StatusCode != HttpStatusCode.OK)
             {
-                try
-                {
-                    var responseMessage = httpClient.GetAsync($@"http://{IpAddress}:44383/api/seats").Result;
-                    if (responseMessage.StatusCode != HttpStatusCode.OK)
-                    {
-                        throw new InvalidOperationException("サーバへの接続に失敗しました。");
-                    }
-
-                    var responseBody = responseMessage.Content.ReadAsStringAsync().Result;
-                    var seatsResponse = JsonConvert.DeserializeObject<dynamic>(responseBody);
-
-                    var seatsResult = new List<Seat>();
-                    foreach (var seat in seatsResponse)
-                    {
-                        seatsResult.Add(new Seat((string)seat.Name, (string)seat.Status));
-                    }
-                    return seatsResult;
-                }
-                catch(HttpRequestException e)
-                {
-                    throw new HttpRequestException("サーバへの接続に失敗しました。");
-                }
-                catch (WebException e)
-                {
-                    throw new WebException("サーバへの接続に失敗しました。");
-                }
-
-                //var seatsResult = JsonConvert.DeserializeObject<dynamic>(responseBody)
-                //    .Select(seat => new Seat(seat.Name, Seat.FromString(seat.Status))
-                //    .ToList();
-
-
+                throw new AggregateException("サーバへの接続に失敗しました。");
             }
 
+            var responseBody = responseMessage.Content.ReadAsStringAsync().Result;
+            var seatsResponse = JsonConvert.DeserializeObject<dynamic>(responseBody);
+
+            return ((IEnumerable<dynamic>)seatsResponse)
+                .Select(seat => new Seat((string)seat.Name, (string)seat.Status))
+                .ToList();
         }
     }
 }
