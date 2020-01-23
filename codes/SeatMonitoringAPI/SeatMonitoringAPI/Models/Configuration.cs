67c51791c18c2a6cf0df9c010fc4c5230f4942be
AukGuisesTike <keisuke.sugita@jp.ricoh.com>
2020-01-23 15:14:45 +0900

    空実装になっていた部分を削除

--- codes/SeatMonitoringAPI/SeatMonitoringAPI/Models/Configuration.cs
+++ codes/SeatMonitoringAPI/SeatMonitoringAPI/Models/Configuration.cs
@@ -25,9 +25,6 @@ namespace SeatMonitoringAPI.Models
                 }
                 return instance;
             }
-            private set
-            {
-            }
         }
         public readonly ReadOnlyCollection<SeatDefinition> seatDefinitions;
 
