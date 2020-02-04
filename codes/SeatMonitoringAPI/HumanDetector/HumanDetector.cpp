d46c61310c73feec70c8c626fe8821911b195c36
AukGuisesTike <keisuke.sugita@jp.ricoh.com>
2020-02-04 10:19:37 +0900

    呼び出し方法を明記

--- codes/SeatMonitoringAPI/HumanDetector/HumanDetector.cpp
+++ codes/SeatMonitoringAPI/HumanDetector/HumanDetector.cpp
@@ -1,16 +1,25 @@
 #include "HumanDetector.h"
 #include <vector>
 #include "opencv2/opencv.hpp"
+#include <windows.h>
+#include <tchar.h>
+#define GetHInstance() ((HINSTANCE)GetModuleHandle("HumanDetector.dll"))
 
 using namespace cv;
 using namespace std;
 
-bool detect(int rows, int cols, unsigned char* image)
+
+extern "C" bool detect(int rows, int cols, unsigned char* image, const char* cascadeFile)
 {
+	char path[MAX_PATH + 1];
+
+	GetModuleFileName(GetHInstance(), path, MAX_PATH);
+
+
 	Mat mat(rows, cols, CV_8UC3, image, Mat::AUTO_STEP);	// 画像データをMat型に変換
 	
 	CascadeClassifier cascade;	//	カスケード分類器格納場所
-	cascade.load("SeatMonitoringAPI\\cascades\\haarcascade_frontalface_alt.xml");	//	正面顔情報が入っているカスケード
+	cascade.load(cascadeFile);	//	正面顔情報が入っているカスケード
 	if (cascade.empty())
 	{
 		throw runtime_error("カスケード分類器が読み込めませんでした。");
--- codes/SeatMonitoringAPI/HumanDetector/HumanDetector.h
+++ codes/SeatMonitoringAPI/HumanDetector/HumanDetector.h
@@ -1,6 +1,8 @@
 #pragma once
 
+using namespace std;
+
 extern "C"
 {
-	__declspec(dllexport) bool detect(int rows, int cols, unsigned char* image);
+	__declspec(dllexport)  bool __stdcall detect(int rows, int cols, unsigned char* image,  const char* cascadeFile);
 }
