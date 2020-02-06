bbc74912a954844128f53d2844dd6f4fc97d435a
AukGuisesTike <keisuke.sugita@jp.ricoh.com>
2020-02-06 16:58:28 +0900

    [障害対応]
    フォントに等幅フォントとして「MS ゴシック」設定

--- src/SeatMonitoringApplicationSln/SeatMonitoringApplication/MainForm.Designer.cs
+++ src/SeatMonitoringApplicationSln/SeatMonitoringApplication/MainForm.Designer.cs
@@ -27,9 +27,13 @@
             // 
             this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1});
+            this.listView1.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.MaximumSize = new System.Drawing.Size(150, 207);
+            this.listView1.MinimumSize = new System.Drawing.Size(150, 207);
+            this.listView1.MultiSelect = false;
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(140, 207);
+            this.listView1.Size = new System.Drawing.Size(150, 207);
             this.listView1.SmallImageList = this.imageList1;
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
@@ -38,7 +42,7 @@
             // columnHeader1
             // 
             this.columnHeader1.Text = "監視結果";
-            this.columnHeader1.Width = 136;
+            this.columnHeader1.Width = 146;
             // 
             // imageList1
             // 
@@ -53,11 +57,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(164, 231);
+            this.ClientSize = new System.Drawing.Size(174, 231);
             this.Controls.Add(this.listView1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.MaximumSize = new System.Drawing.Size(180, 270);
-            this.MinimumSize = new System.Drawing.Size(180, 270);
+            this.MaximumSize = new System.Drawing.Size(190, 270);
+            this.MinimumSize = new System.Drawing.Size(190, 270);
             this.Name = "MainForm";
             this.Text = "座席監視アプリ";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Close);
