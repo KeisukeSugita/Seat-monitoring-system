043d8e79a096565790b01187b65a5b63cfcf178a
AukGuisesTike <keisuke.sugita@jp.ricoh.com>
2020-02-26 14:19:21 +0900

    サーバエラーから復帰したときのテストを実施

--- src/SeatMonitoringApplicationSln/SeatMonitoringApplicationTest/SeatToastNotifierTest.cs
+++ src/SeatMonitoringApplicationSln/SeatMonitoringApplicationTest/SeatToastNotifierTest.cs
@@ -27,7 +27,7 @@ namespace SeatMonitoringApplicationTest
             var expectedXmlDocument1 = new XmlDocument();
             expectedXmlDocument1.LoadXml(new ToastContent()
             {
-                Header = new ToastHeader("0001", "座席監視アプリ", ""),
+                Header = new ToastHeader("Seat Monitoring Application-Toast Notification", "座席監視アプリ", ""),
 
                 Visual = new ToastVisual()
                 {
@@ -53,7 +53,7 @@ namespace SeatMonitoringApplicationTest
             var expectedXmlDocument2 = new XmlDocument();
             expectedXmlDocument2.LoadXml(new ToastContent()
             {
-                Header = new ToastHeader("0001", "座席監視アプリ", ""),
+                Header = new ToastHeader("Seat Monitoring Application-Toast Notification", "座席監視アプリ", ""),
 
                 Visual = new ToastVisual()
                 {
@@ -126,7 +126,7 @@ namespace SeatMonitoringApplicationTest
             var expectedXmlDocument1 = new XmlDocument();
             expectedXmlDocument1.LoadXml(new ToastContent()
             {
-                Header = new ToastHeader("0001", "座席監視アプリ", ""),
+                Header = new ToastHeader("Seat Monitoring Application-Toast Notification", "座席監視アプリ", ""),
 
                 Visual = new ToastVisual()
                 {
@@ -187,11 +187,67 @@ namespace SeatMonitoringApplicationTest
         /// 座席の状態取得に失敗していた状態で、状態取得に成功した通知を受け取った場合のテスト
         /// </summary>
         [TestMethod]
-        public void Notify_IsSucceededFalseToTrue()
+        public void Notify_IsSucceededTrueToFalseToTrue()
         {
+            var expectedSeatToasts1 = new List<(string, string)>();
+            var expectedSeatToasts2 = new List<(string, string)>();
             var resultSeatToasts = new List<(string, string)>();
 
             var callTimes = 0;
+
+            var expectedXmlDocument1 = new XmlDocument();
+            expectedXmlDocument1.LoadXml(new ToastContent()
+            {
+                Header = new ToastHeader("Seat Monitoring Application-Toast Notification", "座席監視アプリ", ""),
+
+                Visual = new ToastVisual()
+                {
+                    BindingGeneric = new ToastBindingGeneric()
+                    {
+                        AppLogoOverride = new ToastGenericAppLogo()
+                        {
+                            Source = $@"{Application.StartupPath}\Icons\サーバ接続エラーアイコン.png",
+                            HintCrop = ToastGenericAppLogoCrop.Default
+                        },
+
+                        Children =
+                            {
+                                new AdaptiveText()
+                                {
+                                    Text = @"サーバへの接続に失敗しました。",
+                                }
+                            }
+                    }
+                }
+            }.GetContent());
+            expectedSeatToasts1.Add((Application.ExecutablePath, expectedXmlDocument1.GetXml()));
+
+            var expectedXmlDocument2 = new XmlDocument();
+            expectedXmlDocument2.LoadXml(new ToastContent()
+            {
+                Header = new ToastHeader("Seat Monitoring Application-Toast Notification", "座席監視アプリ", ""),
+
+                Visual = new ToastVisual()
+                {
+                    BindingGeneric = new ToastBindingGeneric()
+                    {
+                        AppLogoOverride = new ToastGenericAppLogo()
+                        {
+                            Source = $@"{Application.StartupPath}\Icons\サーバ接続エラー復帰アイコン.png",
+                            HintCrop = ToastGenericAppLogoCrop.Default
+                        },
+
+                        Children =
+                            {
+                                new AdaptiveText()
+                                {
+                                    Text = @"サーバ接続エラーから復帰しました。",
+                                }
+                            }
+                    }
+                }
+            }.GetContent());
+            expectedSeatToasts2.Add((Application.ExecutablePath, expectedXmlDocument2.GetXml()));
 
             var toastNotificationManagerMock = new Mock<IToastNotificationManagerWrapper>();
             toastNotificationManagerMock
@@ -204,24 +260,42 @@ namespace SeatMonitoringApplicationTest
 
             // 1回目に通知されるリスト
             var firstTimeSeats = new List<Seat>();
-            firstTimeSeats = null;
+            firstTimeSeats.Add(new Seat("佐藤　太郎", Seat.SeatStatus.Exists));
+            firstTimeSeats.Add(new Seat("さとう　たろう", Seat.SeatStatus.NotExists));
+            firstTimeSeats.Add(new Seat("サトウ　タロウ", Seat.SeatStatus.Exists));
 
             // 2回目に通知されるリスト
             var secondTimeSeats = new List<Seat>();
-            secondTimeSeats.Add(new Seat("佐藤　太郎", Seat.SeatStatus.Exists));
-            secondTimeSeats.Add(new Seat("さとう　たろう", Seat.SeatStatus.NotExists));
-            secondTimeSeats.Add(new Seat("サトウ　タロウ", Seat.SeatStatus.Exists));
+            secondTimeSeats = null;
+
+            // 3回目に通知されるリスト
+            var thirdTimeSeats = new List<Seat>();
+            thirdTimeSeats.Add(new Seat("佐藤　太郎", Seat.SeatStatus.Exists));
+            thirdTimeSeats.Add(new Seat("さとう　たろう", Seat.SeatStatus.Exists));
+            thirdTimeSeats.Add(new Seat("サトウ　タロウ", Seat.SeatStatus.Exists));
 
             var seatToastNotifier = new SeatToastNotifier(toastNotificationManagerMock.Object, Application.ExecutablePath);
 
-            seatToastNotifier.Notify(firstTimeSeats, false);
+            seatToastNotifier.Notify(firstTimeSeats, true);
             Assert.AreEqual(resultSeatToasts.Count, 0);
             Assert.AreEqual(callTimes, 0);
 
+            // サーバ接続エラーの場合のAssert
+            seatToastNotifier.Notify(secondTimeSeats, false);
+            Assert.AreEqual(resultSeatToasts.Count, 1);
+            Assert.AreEqual(callTimes, 1);
+
+            CollectionAssert.AreEqual(expectedSeatToasts1, resultSeatToasts);
+
+            resultSeatToasts.Clear();
+            callTimes = 0;
+
             // サーバ接続エラーから復帰した場合のAssert
-            seatToastNotifier.Notify(secondTimeSeats, true);
-            Assert.AreEqual(resultSeatToasts.Count, 0);
-            Assert.AreEqual(callTimes, 0);
+            seatToastNotifier.Notify(thirdTimeSeats, true);
+            Assert.AreEqual(resultSeatToasts.Count, 1);
+            Assert.AreEqual(callTimes, 1);
+
+            CollectionAssert.AreEqual(expectedSeatToasts2, resultSeatToasts);
         }
     }
 }
