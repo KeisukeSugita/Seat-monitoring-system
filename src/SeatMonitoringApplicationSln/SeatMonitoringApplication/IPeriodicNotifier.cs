089d32c8319c1d8a2cf1b6b7bbdaa59defb2bab1
AukGuisesTike <keisuke.sugita@jp.ricoh.com>
2020-02-26 11:36:36 +0900

    誤解を生むコメントの修正

--- src/SeatMonitoringApplicationSln/SeatMonitoringApplication/IPeriodicNotifier.cs
+++ src/SeatMonitoringApplicationSln/SeatMonitoringApplication/IPeriodicNotifier.cs
@@ -7,19 +7,24 @@ using System.Threading.Tasks;
 namespace SeatMonitoringApplication
 {
     /// <summary>
-    /// 指定されたメソッド(Destination)に結果を通知するインターフェース
+    /// 指定されたメソッド(Destination)に<see cref="SeatMonitoringApiClient.GetSeats"/>の戻り値を通知するインターフェース
     /// </summary>
     public interface IPeriodicNotifier
     {
+        /// <summary>
+        ///  Startメソッドの中で定期的に実行されるメソッド
+        ///  座席の状態を通知させたいメソッドを登録できる
+        /// </summary>
         Action<List<Seat>, bool> Destination { get; set; }
 
         /// <summary>
         /// 座席状態の取得と結果の通知を定期的に非同期に行うメソッド
+        /// Stopメソッドを呼び出すことで非同期処理を終了させる
         /// </summary>
         void Start();
 
         /// <summary>
-        /// Startメソッドを終了させるメソッド
+        /// Startメソッドで非同期に実行されている処理を終了させるメソッド
         /// </summary>
         void Stop();
     }
